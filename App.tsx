bd2a071f8ee4b4d8a7aa7927a6ea3ffa486a9df8
richardHaggioGwati <haggiog0844@gmail.com>
2023-12-24 15:24:48 +0200

    KeyboardAvoidingView

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MapScreen from "./screens/MapScreen";
 import EatsScreen from "./screens/EatsScreen";
+import {KeyboardAvoidingView, Platform} from "react-native";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,11 +15,14 @@ export default function App() {
         <NavigationContainer>
             <Provider store={store}>
                 <SafeAreaProvider>
-                    <Stack.Navigator>
-                        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false}} />
-                        <Stack.Screen name="MapScreen" component={MapScreen} options={{headerShown: false}} />
-                        <Stack.Screen name="EatsScreen" component={EatsScreen} options={{headerShown: false}} />
-                    </Stack.Navigator>
+                    <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}
+                                          keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0} style={{flex: 1}}>
+                        <Stack.Navigator>
+                            <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false}}/>
+                            <Stack.Screen name="MapScreen" component={MapScreen} options={{headerShown: false}}/>
+                            <Stack.Screen name="EatsScreen" component={EatsScreen} options={{headerShown: false}}/>
+                        </Stack.Navigator>
+                    </KeyboardAvoidingView>
                 </SafeAreaProvider>
             </Provider>
         </NavigationContainer>
