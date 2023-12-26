5eb4517839fc31456e2257a2dc44bc425faeb508
richardHaggioGwati <haggiog0844@gmail.com>
2023-12-26 14:48:49 +0200

    home_button

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,15 +1,25 @@
-import {View} from "react-native";
+import {TouchableOpacity, View} from "react-native";
 import tw from "tailwind-react-native-classnames";
 import Map from "../components/Map";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import NavigateCard from "../components/NavigateCard";
 import RideOptionsCard from "../components/RideOptionsCard";
+import {Icon} from "react-native-elements";
+import {useNavigation} from "@react-navigation/native";
 
 const MapScreen = () => {
     const Stack = createNativeStackNavigator()
+    const navigation = useNavigation()
 
     return (
         <View>
+            <TouchableOpacity style={tw`bg-gray-100 absolute top-16 left-8 z-50 p-3 rounded-full shadow-lg`} onPress={() => {
+
+                // @ts-ignore
+                navigation.navigate('HomeScreen')}}>
+                <Icon name="home" />
+            </TouchableOpacity>
+
             <View style={tw`h-1/2`}>
                 <Map />
             </View>
